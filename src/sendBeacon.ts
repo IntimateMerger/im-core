6001e49c61a29391a62ee0996de381c70bb9a123
higoshi <h33je5sg@gmail.com>
2023-01-20 17:04:48 +0900

    fixed JSDoc

--- src/sendBeacon.ts
+++ src/sendBeacon.ts
@@ -4,8 +4,8 @@ type Body = Blob | string | FormData | URLSearchParams;
 
 /**
  * navigator.sendBeaconを糖衣する関数で、sendBeaconが使用できない場合XHRにフォールバックします。
- * @param url
- * @param body {Blob | string | FormData | URLSearchParams} 型によってContent-Typeとペイロードが変化します。Blob型はそのtypeに応じ、stirngはtext/plain、FormDataはmultipart/form-data、URLSearchParamsはapplication/x-www-form-urlencodedとして送信されます。
+ * @param {string} url
+ * @param {(Blob | string | FormData | URLSearchParams)} body 型によってContent-Typeとペイロードが変化します。Blob型はそのtypeに応じ、stirngはtext/plain、FormDataはmultipart/form-data、URLSearchParamsはapplication/x-www-form-urlencodedとして送信されます。
  * @returns {boolean}
  */
 export function sendBeacon(url: string, body?: Body): boolean {
@@ -25,8 +25,9 @@ type AnyObjects = Record<string, unknown>; // ≒ {}
 
 /**
  * データをURLSearchParamsに変換し、sendBeacon関数を実行、application/x-www-form-urlencodedとしてデータを送信します。
- * @param url
- * @param data
+ * @template T - extends Record<string, unknown>
+ * @param {string} url
+ * @param {T} data
  * @returns {boolean}
  */
 export function sendBeaconAsXWwwUrlEncoded<T extends AnyObjects>(
@@ -43,9 +44,10 @@ export function sendBeaconAsXWwwUrlEncoded<T extends AnyObjects>(
 
 /**
  * データをFormDataに変換し、sendBeacon関数を実行、multipart/form-dataとしてデータを送信します。
- * @param url
- * @param data
- * @returns
+ * @template T - extends Record<string, unknown>
+ * @param {string} url
+ * @param {T} data
+ * @returns {boolean}
  */
 export function sendBeaconAsMultipartFormData<T extends AnyObjects>(
   url: string,
@@ -60,10 +62,11 @@ export function sendBeaconAsMultipartFormData<T extends AnyObjects>(
 }
 
 /**
- *
- * @param url データをBlob(type: 'application/json')に変換し、sendBeacon関数を実行、application/jsonとしてデータを送信します。
- * @param data
- * @returns
+ * データをJSON.stringifyしたBlobに変換し、sendBeacon関数を実行、application/jsonとしてデータを送信します。
+ * @template T - extends Record<string, unknown>
+ * @param {string} url
+ * @param {T} data
+ * @returns {boolean}
  */
 export function sendBeaconAsJson<T extends AnyObjects>(url: string, data: T) {
   const blob = new Blob([JSON.stringify(data)], {type: 'application/json'});
