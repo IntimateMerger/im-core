0aea237e6b6e9bc3c5b79c760b71444b76b7ca3f
higoshi <h33je5sg@gmail.com>
2023-04-20 18:01:16 +0900

    withCredentials

--- src/sendBeacon.ts
+++ src/sendBeacon.ts
@@ -12,7 +12,7 @@ export function sendBeacon(url: string, body?: Body): boolean {
     return navigator.sendBeacon(url, body);
   } else {
     try {
-      post(url, body, void 0, {asynchronous: false});
+      post(url, body, void 0, {asynchronous: false, withCredentials: true});
       return true;
     } catch {
       return false;
